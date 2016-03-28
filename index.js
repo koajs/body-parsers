364bd297df36eb4ad37c3f211a20c735276a2686
jongleberry <me@jongleberry.com>
2016-03-28 14:39:01 -0700

    lint++

--- index.js
+++ index.js
@@ -4,83 +4,83 @@
  * To do: move most of the body parsing stuff to a separate library.
  */
 
-const get = require('raw-body');
-const qs = require('querystring');
+const get = require('raw-body')
+const qs = require('querystring')
 
-module.exports = app => {
-  Object.keys(request).forEach(key => {
-    app.request[key] = request[key];
-  });
-  Object.keys(response).forEach(key => {
-    app.response[key] = response[key];
-  });
-  Object.keys(context).forEach(key => {
-    app.context[key] = context[key];
-  });
+module.exports = (app) => {
+  Object.keys(request).forEach((key) => {
+    app.request[key] = request[key]
+  })
+  Object.keys(response).forEach((key) => {
+    app.response[key] = response[key]
+  })
+  Object.keys(context).forEach((key) => {
+    app.context[key] = context[key]
+  })
   return app
 }
 
-const context = {};
-const request = {};
-const response = {};
+const context = {}
+const request = {}
+const response = {}
 
 request.json = function (limit) {
-  if (!this.length) return Promise.resolve();
-  return this.text(limit).then(text => this._parse_json(text));
+  if (!this.length) return Promise.resolve()
+  return this.text(limit).then((text) => this._parse_json(text))
 }
 
 request._parse_json = function (text) {
   if (this.app.jsonStrict !== false) {
-    text = text.trim();
-    const first = text[0];
+    text = text.trim()
+    const first = text[0]
     if (first !== '{' && first !== '[') {
-      this.ctx.throw(400, 'only json objects or arrays allowed');
+      this.ctx.throw(400, 'only json objects or arrays allowed')
     }
   }
   try {
-    return JSON.parse(text);
+    return JSON.parse(text)
   } catch (err) {
-    this.ctx.throw(400, 'invalid json received');
+    this.ctx.throw(400, 'invalid json received')
   }
 }
 
 request.urlencoded = function (limit) {
-  if (!this.length) return Promise.resolve();
-  return this.text(limit).then(text => this._parse_urlencoded(text));
+  if (!this.length) return Promise.resolve()
+  return this.text(limit).then((text) => this._parse_urlencoded(text))
 }
 
 request._parse_urlencoded = function (text) {
-  const parse = (this.app.querystring || qs).parse;
+  const parse = (this.app.querystring || qs).parse
   try {
-    return parse(text);
+    return parse(text)
   } catch (err) {
-    this.ctx.throw(400, 'invalid urlencoded received');
+    this.ctx.throw(400, 'invalid urlencoded received')
   }
 }
 
 request.text = function (limit) {
-  this.response.writeContinue();
+  this.response.writeContinue()
   return get(this.req, {
     limit: limit || '100kb',
     length: this.length,
-    encoding: 'utf8',
+    encoding: 'utf8'
   })
 }
 
 request.buffer = function (limit) {
-  this.response.writeContinue();
+  this.response.writeContinue()
   return get(this.req, {
     limit: limit || '1mb',
-    length: this.length,
+    length: this.length
   })
 }
 
 response.writeContinue = function () {
   if (!this._checkedContinue && this.req.checkContinue) {
-    this.res.writeContinue();
-    this._checkedContinue = true;
+    this.res.writeContinue()
+    this._checkedContinue = true
   }
-  return this;
+  return this
 }
 
-context.save = require('fs-cp');
+context.save = require('fs-cp')
--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 'use strict'
 
+/* eslint-env mocha */
+
 const request = require('supertest')
 const assert = require('assert')
 const http = require('http')
@@ -7,121 +9,121 @@ const koa = require('koa')
 
 describe('Body Parsing', () => {
   describe('.request.json()', () => {
-    it('should parse a json body', done => {
+    it('should parse a json body', (done) => {
       const app = koala()
-      app.use(function* () {
+      app.use(function * () {
         this.body = yield this.request.json()
       })
       request(app.listen())
-      .post('/')
-      .send({
-        message: 'lol'
-      })
-      .expect(200)
-      .expect(/"message"/)
-      .expect(/"lol"/, done)
+        .post('/')
+        .send({
+          message: 'lol'
+        })
+        .expect(200)
+        .expect(/"message"/)
+        .expect(/"lol"/, done)
     })
 
-    it('should throw on non-objects in strict mode', done => {
+    it('should throw on non-objects in strict mode', (done) => {
       const app = koala()
-      app.use(function* () {
+      app.use(function * () {
         this.body = yield this.request.json()
       })
       request(app.listen())
-      .post('/')
-      .type('json')
-      .send('"lol"')
-      .expect(400, done)
+        .post('/')
+        .type('json')
+        .send('"lol"')
+        .expect(400, done)
     })
 
-    it('should not throw on non-objects in non-strict mode', done => {
+    it('should not throw on non-objects in non-strict mode', (done) => {
       const app = koala()
       app.jsonStrict = false
-      app.use(function* () {
+      app.use(function * () {
         this.body = yield this.request.json()
       })
       request(app.listen())
-      .post('/')
-      .type('json')
-      .send('"lol"')
-      .expect(200)
-      .expect('lol', done)
+        .post('/')
+        .type('json')
+        .send('"lol"')
+        .expect(200)
+        .expect('lol', done)
     })
   })
 
   describe('.request.urlencoded()', () => {
-    it('should parse a urlencoded body', done => {
+    it('should parse a urlencoded body', (done) => {
       const app = koala()
-      app.use(function* () {
+      app.use(function * () {
         this.body = yield this.request.urlencoded()
       })
       request(app.listen())
-      .post('/')
-      .send("message=lol")
-      .expect(200)
-      .expect(/"message"/)
-      .expect(/"lol"/, done)
+        .post('/')
+        .send('message=lol')
+        .expect(200)
+        .expect(/"message"/)
+        .expect(/"lol"/, done)
     })
   })
 
   describe('.request.text()', () => {
-    it('should get the raw text body', done => {
+    it('should get the raw text body', (done) => {
       const app = koala()
-      app.use(function* () {
+      app.use(function * () {
         this.body = yield this.request.text()
         assert.equal('string', typeof this.body)
       })
       request(app.listen())
-      .post('/')
-      .send("message=lol")
-      .expect(200)
-      .expect('message=lol', done)
+        .post('/')
+        .send('message=lol')
+        .expect(200)
+        .expect('message=lol', done)
     })
 
-    it('should throw if the body is too large', done => {
-      const app = koala();
-      app.use(function* () {
+    it('should throw if the body is too large', (done) => {
+      const app = koala()
+      app.use(function * () {
         yield this.request.text('1kb')
         this.body = 204
       })
       request(app.listen())
-      .post('/')
-      .send(new Buffer(2048))
-      .expect(413, done)
+        .post('/')
+        .send(new Buffer(2048))
+        .expect(413, done)
     })
   })
 
   describe('.request.buffer()', () => {
-    it('should get the raw buffer body', done => {
+    it('should get the raw buffer body', (done) => {
       const app = koala()
-      app.use(function* () {
+      app.use(function * () {
         this.body = yield this.request.buffer()
         assert(Buffer.isBuffer(this.body))
       })
       request(app.listen())
-      .post('/')
-      .send("message=lol")
-      .expect(200)
-      .expect('message=lol', done)
+        .post('/')
+        .send('message=lol')
+        .expect(200)
+        .expect('message=lol', done)
     })
 
-    it('should throw if the body is too large', done => {
-      const app = koala();
-      app.use(function* () {
+    it('should throw if the body is too large', (done) => {
+      const app = koala()
+      app.use(function * () {
         yield this.request.buffer('1kb')
         this.body = 204
       })
       request(app.listen())
-      .post('/')
-      .send(new Buffer(2048))
-      .expect(413, done)
+        .post('/')
+        .send(new Buffer(2048))
+        .expect(413, done)
     })
   })
 
   describe('Expect: 100-continue', () => {
-    it('should send 100-continue', done => {
-      const app = koala();
-      app.use(function* () {
+    it('should send 100-continue', (done) => {
+      const app = koala()
+      app.use(function * () {
         this.body = yield this.request.json()
       })
       app.listen(function () {
@@ -133,15 +135,15 @@ describe('Body Parsing', () => {
             'content-type': 'application/json'
           }
         })
-        .once('continue', function () {
-          this.end(JSON.stringify({
-            message: 'lol'
-          }))
-        })
-        .once('response', res => {
-          done()
-        })
-        .once('error', done)
+          .once('continue', function () {
+            this.end(JSON.stringify({
+              message: 'lol'
+            }))
+          })
+          .once('response', (res) => {
+            done()
+          })
+          .once('error', done)
       })
     })
   })
